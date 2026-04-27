d18dd07c9a7e1ebcbe8fd6f4fcecf23a31a7c467
AndreZaoo0 <andrecarlosdasilva.1310@gmail.com>
2026-04-27 19:36:53 -0300

    Uptade

--- SuperTrunfo/Operadores/modificadores/unsigned-long-int.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
- 
-int main() {
-    unsigned long int numeroPositivoGrande = 4000000000;
-    printf("Número positivo grande: %lu\n", numeroPositivoGrande);
- 
-    return 0;
-}
